912437881ab9b20b50d7bfaf38ec86d4c0ba3e85
rzastwn <rezastwn2004@gmail.com>
2025-07-16 19:46:22 +0700

    Modify routes and add PostAdmin Controller

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -11,3 +11,12 @@ $routes->get('/contact', 'Page::contact');
 $routes->get('/faqs', 'Page::faqs');
 $routes->get('/post', 'Post::index');
 $routes->get('/post/(:any)', 'Post::viewPost/$1');
+
+//Admin Post
+$routes->group('admin', function($routes){
+    $routes->get('post', 'PostAdmin::index');
+    $routes->get('post/(:segment)/preview', 'PostAdmin::preview/$1');
+$routes->add('post/new', 'PostAdmin::create');
+    $routes->add('post/(:segment)/edit', 'PostAdmin::edit/$1');
+    $routes->get('post/(:segment)/delete', 'PostAdmin::delete/$1');
+});
