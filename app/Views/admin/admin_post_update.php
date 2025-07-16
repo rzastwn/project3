a3a650bd505661e45b9d003df29a54436dc6a69c
rzastwn <rezastwn2004@gmail.com>
2025-07-16 20:42:33 +0700

    add view admin_post_update

--- app/Views/admin/admin_post_update.php
+++ app/Views/admin/admin_post_update.php
@@ -39,26 +39,32 @@
 
     <div class="p-5 mb-4 bg-light rounded-3">
         <div class="container py-5">
-            <h1 class="display-5 fw-bold">Post > New</h1>
+            <h1 class="display-5 fw-bold">Post > Update</h1>
         </div>
     </div>
 
     <div class="container">
  <form action="" method="post" id="text-editor">
+ <input type="hidden" name="id" value="<?=
+$post['id'] ?>" />
  <div class="form-group mb-2">
  <label for="title">Title</label>
- <input type="text" name="title" class="form-control"
-placeholder="Post title" required>
+ <input type="text" name="title" class="formcontrol"
+ placeholder="Post title" value="<?=
+$post['title'] ?>" required>
  </div>
  <div class="form-group mb-2">
- <textarea name="content" class="form-control" cols="30"
-rows="10" placeholder="Write a great post!"></textarea>
+ <textarea name="content"
+ class="form-control"
+ cols="30" rows="10"
+ placeholder="Write a great post!"><?=
+$post['content'] ?></textarea>
  </div>
- <div class="form-group">
- <button type="submit" name="status" value="published"
-class="btn btn-primary">Publish</button>
- <button type="submit" name="status" value="draft" class="btn
-btn-secondary">Save to Draft</button>
+ <div class="form-group mb-2">
+ <button type="submit" name="status"
+value="published" class="btn btn-primary">Publish</button>
+ <button type="submit" name="status" value="draft"
+class="btn btn-secondary">Save to Draft</button>
  </div>
  </form>
 </div>
